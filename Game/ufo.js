f470d88fe38675f74629e531619d30f27c3da230
Akihiro Yamamoto <akiyam0613@gmail.com>
2018-01-11 20:39:46 +0900

    3-11 上下移動処理を追加

--- Game/ufo.js
+++ Game/ufo.js
@@ -4,8 +4,8 @@ var bInitFlg = true;    //初期実行処理FLG
 //キー押下処理を追加
 function init()
 {
-    window.addEventListener("keydown", keydown);        //キー押下イベント実装
-    ufo = new UFO(document.getElementById("ufo"), 100); //オブジェクト宣言
+    window.addEventListener("keydown", keydown);                //キー押下イベント実装
+    ufo = new UFO(document.getElementById("ufo"), 100, 50);     //オブジェクト宣言
 }
 
 //キー押下処理
@@ -15,24 +15,34 @@ function keydown(e)
     {
         ufo.moveLeft();
     }
+    else if (e.keyCode == 38)    //上カーソル
+    {
+        ufo.moveUp();
+    }
     else if (e.keyCode == 39)   //右カーソル
     {
         ufo.moveRight();
+    }
+    else if (e.keyCode == 40)   //下カーソル
+    {
+        ufo.moveDown();
     }
 }
 
 //オブジェクトの処理
 //_elem:Element:対象オブジェクト
 //_xpos:X Position:X座標
-function UFO(_elem, _xpos)
+function UFO(_elem, _xpos, _ypos)
 {
     this.elem = _elem;  //対象オブジェクト
     this.xpos = _xpos;  //対象X座標
+    this.ypos = _ypos;  //対象Y座標
     
     //初期位置移動
     if (bInitFlg == true)
     {
         this.elem.style.left = this.xpos + "px";
+        this.elem.style.top = this.ypos + "px";
         bInitFlg = false;
     }
 
@@ -42,6 +52,7 @@ function UFO(_elem, _xpos)
         //左へ10px移動
         this.xpos -= 10;
         this.elem.style.left = this.xpos + "px";
+        this.elem.style.top = this.ypos + "px";
     }
 
     //右カーソル処理
@@ -50,6 +61,25 @@ function UFO(_elem, _xpos)
         //右へ10px移動
         this.xpos += 10;
         this.elem.style.left = this.xpos + "px";
+        this.elem.style.top = this.ypos + "px";
+    }
+
+    //上カーソル処理
+    this.moveUp = function()
+    {
+        //上へ5px移動
+        this.ypos -= 10;
+        this.elem.style.left = this.xpos + "px";
+        this.elem.style.top = this.ypos + "px";
+    }
+
+    //下カーソル処理
+    this.moveDown = function()
+    {
+        //下へ5px移動
+        this.ypos += 10;
+        this.elem.style.left = this.xpos + "px";
+        this.elem.style.top = this.ypos + "px";
     }
 
 }
