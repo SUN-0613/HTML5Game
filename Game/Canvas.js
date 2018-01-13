338c11f8a71917482efeafa5366592134d415d16
Akihiro Yamamoto <akiyam0613@gmail.com>
2018-01-13 20:45:38 +0900

    4-2-3 円の描画
    例文記述

--- Game/Canvas.js
+++ Game/Canvas.js
@@ -59,7 +59,7 @@ function init()
 
     /* 4-2-2 矩形の描画
        矩形描画パターン
-    */
+
     ctx.strokeStyle = "#FF0000";
     ctx.fillStyle = "#00FFFF";
     ctx.lineWidth = 5;
@@ -73,5 +73,41 @@ function init()
     //3つ目:塗りつぶした内の一部をクリア
     ctx.fillRect(300, 30, 80, 50);
     ctx.clearRect(320, 50, 50, 20);
+
+    */
+
+    /* 4-2-3 円の描画
+       円描画パターン
+    */
+    ctx.strokeStyle = "#FF0000";
+    ctx.fillStyle = "#00FFFF";
+    ctx.lineWidth = 5;
+
+    //1つ目
+    ctx.beginPath();
+    ctx.arc(100, 50, 30, 0, 2 * Math.PI);  //始点X, 始点Y, 半径, 初期Radian, 終期Radian, [反時計回りFLG] []は省略項目
+    ctx.closePath();
+    ctx.fill();     //塗りつぶし描画
+
+    //2つ目
+    ctx.beginPath();
+    ctx.moveTo(200, 50);
+    ctx.arc(200, 50, 30, 0, Math.PI / 3);
+    ctx.closePath();
+    ctx.stroke();   //描画
+
+    //3つ目
+    ctx.beginPath();
+    ctx.moveTo(300, 50);
+    ctx.arc(300, 50, 30, 0, Math.PI / 3, true);
+    ctx.closePath();
+    ctx.stroke();
+
+    //4つ目
+    ctx.beginPath();
+    ctx.moveTo(400, 50);
+    ctx.arc(400, 50, 30, -1 * Math.PI / 6, Math.PI / 6, true);
+    ctx.closePath();
+    ctx.fill();
 
 }
