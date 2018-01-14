f5b02d26616430c32908e7a6942803566f1cb328
Akihiro Yamamoto <akiyam0613@gmail.com>
2018-01-14 20:46:53 +0900

    4-2 演習 パックマン

---
+++ Game/Packman.js
@@ -0,0 +1,20 @@
+﻿var ctx;
+
+function init()
+{
+
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var dMouth = (1 / 6) * Math.PI;     //口半分の角度[rad]
+
+    ctx.fillStyle = "#FFFF00";
+    ctx.lineWidth = 1;
+
+    //初期表示
+    ctx.beginPath();
+    ctx.moveTo(400, 300);
+    ctx.arc(400, 300, 50, (-1) * dMouth, dMouth, true);
+    ctx.closePath();
+    ctx.fill();
+
+}
