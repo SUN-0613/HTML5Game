12d2e915a3abdb40bcd696bfee042b1fd5694303
Akihiro Yamamoto <akiyam0613@gmail.com>
2018-01-17 19:39:35 +0900

    4-5 演習 文字盤描画

--- Game/Clock.js
+++ Game/Clock.js
@@ -5,20 +5,36 @@
     var ctx = canvas.getContext("2d");
     var iLoop;
     var dR;
+    var iStart;
+    var iFinish;
 
-    for (iLoop = 0; iLoop < 12; iLoop++)
+    for (iLoop = 0; iLoop < 60; iLoop++)
     {
 
         ctx.save();                 //座標系を保存・・・(1)
 
-        dR = Math.PI / 6 * iLoop;
+        dR = Math.PI / 30 * iLoop;
+        
+        if (iLoop % 5 == 0)
+        {
+            iStart = -105;
+            iFinish = -65;
+            ctx.lineWidth = 2;
+        }
+        else
+        {
+            iStart = -100;
+            iFinish = -70;
+            ctx.lineWidth = 1;
+        }
 
-        ctx.translate(100, 100);    //座標系を(x,y)だけ移動
+        ctx.translate(200, 200);    //座標系を(x,y)だけ移動
         ctx.rotate(dR);             //座標系を(r)回転
 
         ctx.beginPath();
-        ctx.moveTo(0, -60);
-        ctx.lineTo(0, -50);
+        ctx.moveTo(0, iStart);
+        ctx.lineTo(0, iFinish);
+        
         ctx.stroke();
         ctx.restore();              //座標系を保存した(1)へ復元
 
