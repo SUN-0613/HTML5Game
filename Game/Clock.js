00d0fb9eaef113397873d0f5f1a2ccbb4aeaf60f
Akihiro Yamamoto <akiyam0613@gmail.com>
2018-01-17 15:02:56 +0900

    4-5 座標系 コメント追加

--- Game/Clock.js
+++ Game/Clock.js
@@ -9,18 +9,18 @@
     for (iLoop = 0; iLoop < 12; iLoop++)
     {
 
-        ctx.save();
+        ctx.save();                 //座標系を保存・・・(1)
 
         dR = Math.PI / 6 * iLoop;
 
-        ctx.translate(100, 100);
-        ctx.rotate(dR);
+        ctx.translate(100, 100);    //座標系を(x,y)だけ移動
+        ctx.rotate(dR);             //座標系を(r)回転
 
         ctx.beginPath();
         ctx.moveTo(0, -60);
         ctx.lineTo(0, -50);
         ctx.stroke();
-        ctx.restore();
+        ctx.restore();              //座標系を保存した(1)へ復元
 
     }
 
