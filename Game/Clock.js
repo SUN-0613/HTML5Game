759cbe672e51fdf8fa0ab47c64b8dcb9713aa414
Akihiro Yamamoto <akiyam0613@gmail.com>
2018-01-17 14:59:27 +0900

    4-5 座標系
    時計描画

---
+++ Game/Clock.js
@@ -0,0 +1,27 @@
+﻿function init()
+{
+    
+    var canvas = document.getElementById("can");
+    var ctx = canvas.getContext("2d");
+    var iLoop;
+    var dR;
+
+    for (iLoop = 0; iLoop < 12; iLoop++)
+    {
+
+        ctx.save();
+
+        dR = Math.PI / 6 * iLoop;
+
+        ctx.translate(100, 100);
+        ctx.rotate(dR);
+
+        ctx.beginPath();
+        ctx.moveTo(0, -60);
+        ctx.lineTo(0, -50);
+        ctx.stroke();
+        ctx.restore();
+
+    }
+
+}
