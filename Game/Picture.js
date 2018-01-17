bfddd261a9b547e1ab0af352ea616a078f2b56e0
Akihiro Yamamoto <akiyam0613@gmail.com>
2018-01-17 14:48:18 +0900

    4-4 画像
    DTBの画像をcanvasに表示

---
+++ Game/Picture.js
@@ -0,0 +1,10 @@
+﻿function init()
+{
+
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var pic = document.getElementById("pic");
+
+    ctx.drawImage(pic, 10, 10, 727, 1024, 100, 100, 400, 700);
+
+}
